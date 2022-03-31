60a6b2aa2664cd2411929ebb91a06e9c57b7bc48
Logon Anália Franco <logonaf@cds.afc.lab>
2022-03-30 21:21:50 -0300

    exe aula quarta

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Aula-python
+
+https://docs.google.com/forms/d/e/1FAIpQLSfU2Q6R_nQf83OJMjDdhADExNM0t_7--4BORpi8pDhhuP9xiQ/formResponse
---
+++ aula.py
@@ -0,0 +1,59 @@
+# altura = float(input("Qual a sua altura?"))
+# peso = float(input("Qual o seu peso?"))
+
+# resultado = peso / (altura + altura)
+
+# if resultado < 18.5:
+#     print(resultado)
+#     print("Abaixo do peso ideal")
+# elif resultado >= 18.5 and resultado < 24.9:
+#     print(resultado)
+#     print("Peso normal")
+# elif resultado >= 25 and resultado < 29.9:
+#     print(resultado)
+#     print("Sobrepeso")
+# elif resultado >= 30 and resultado < 34.9:
+#     print(resultado)
+#     print("Obesidade Grau I")
+# elif resultado >= 35 and resultado < 39.9:
+#     print(resultado)
+#     print("Obesidade Grau II")
+# elif resultado >= 40:
+#     print(resultado)
+#     print("Obesidade Gurau III")
+
+# if altura < 1.60:
+#     print("Você é baixo")
+# elif altura >= 1.60:
+#     print("Você é alto")
+
+# if peso < 65:
+#     print("A pessoa é magra")
+# elif peso >= 65:
+#     print("Pessoa acima do peso")
+
+#################################################
+
+#################################################
+
+# numero = int(input("Escolha um número"))
+
+# if numero > 10:
+#     resultado = numero * 3
+#     print("O resultado do seu número * 3 é" , resultado)
+# else:
+#     resultado = numero / 2
+#     print("O seu número / por 2 é" , resultado)
+
+##############################################################
+
+#######################################################
+
+# numero = int(input("Escolha um número"))
+
+# if (numero%2) == 0:
+#     resultado = numero * 2
+#     print(resultado , "É o seu número")
+# else:
+#     resultado = numero * 5
+#     print(resultado , "É o seu número")
